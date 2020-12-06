8146159188d7fb1331be5b63c41c6d578b0cbd9e
Victor Santos Silva <victor.s.silva@unesp.br>
2020-12-06 13:46:21 -0300

    atualiza

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return'<h1>Bem vindo ao Flask</h1>'
+    return'<h1>Bem vindo ao Flask^^</h1>'
 
 if __name__ == '__main__':
     app.run(debug=True)
